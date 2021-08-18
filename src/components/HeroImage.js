d626c9e13284c5a47f93715cf4914d35aa32a585
Candelaojeda <ojedacandelaco@gmail.com>
2021-08-18 03:49:23 -0300

    added HeroImage component

---
+++ src/components/HeroImage.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function HeroImage() {
+  return (
+    <>
+      <article className="heroImage">
+        <div>
+          <h1>¿Qué hacemos?</h1>
+        </div>
+        <p>
+          Nos dedicamos a diseñar y fabricar objetos de concreto funcionales.
+          Todos nuestros productos únicos ofrecen acabados artesanales altamente
+          refinados. Los productos Luna tienen variaciones sutiles en sus formas
+          para producir un aspecto orgánico y natural que seguramente complacerá
+          incluso al ojo más exigente. Hemos hecho todo lo posible para crear
+          productos únicos. Nos encontramos en Córdoba, Argentina y realizamos
+          envíos a todo el país.
+        </p>
+      </article>
+    </>
+  );
+}
