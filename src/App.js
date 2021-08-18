1679a2bdcdf59460463cb66050b18977b5582e20
Candelaojeda <ojedacandelaco@gmail.com>
2021-08-18 02:06:27 -0300

    added Introduction component in app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Introduction from './components/Introduction';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar/>
+        <Introduction/>
         <Switch>
           <Route>
-            
+
           </Route>
         </Switch>
       </Router>
