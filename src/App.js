c0e8f3b9929c781f79ddebfab0f4a79d61dacd84
Candelaojeda <ojedacandelaco@gmail.com>
2021-08-18 00:30:00 -0300

    import BrowserRouter, Route and Switch in app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route>
+            
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
