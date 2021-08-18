a14cdb1f00bdb8b776eda0b897a33c6446d97468
Candelaojeda <ojedacandelaco@gmail.com>
2021-08-18 00:27:22 -0300

    added contents Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navbar-text">
+      <nav>
+        <div>
+          <h7>Página Oficial Luna Arte & Cemento </h7>
+        </div>
+      </nav>
+    </div>
+  );
+}
